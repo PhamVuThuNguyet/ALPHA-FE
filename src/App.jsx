143564348d742cb402200d1151d58e45a84e7041
PhamVuThuNguyet <pvtnguyet.19it1@vku.udn.vn>
2022-02-28 21:50:42 +0700

    Update folder name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import SideBar from './components/layouts/header/SideBar';
+import SideBar from './components/layouts/sidebar/SideBar';
 function App() {
   return (
     <div className="App">

cd054e335818ef20580ea504645254fe9b1f357f
PhamVuThuNguyet <pvtnguyet.19it1@vku.udn.vn>
2022-02-28 15:38:29 +0700

    update margin

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -14,7 +14,7 @@ class Map extends React.Component {
                 <Grid item xs={8}>
                     <MainMap googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyBcJ_wdqHZZFttshBqyE_f0wfVDa4bjgt8&callback=initMap`}
                         loadingElement={<div style={{ height: `100%` }} />}
-                        containerElement={<div style={{ height: `90vh`, margin: `auto`, border: '2px solid black' }} />}
+                        containerElement={<div style={{ height: `90vh`, margin: `3vh`, border: '2px solid black' }} />}
                         mapElement={<div style={{ height: `100%` }} />} />
                 </Grid>
                 <Grid item xs={2}>
